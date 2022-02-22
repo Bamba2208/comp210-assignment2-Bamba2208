6df26629d7e4cbca24aedb78e0fe6dc512528b77
Ahmadou Diouf <adiouf@unc.edu>
2022-02-21 20:38:03 -0500

    ju

--- assn2/ArrayList.java
+++ assn2/ArrayList.java
@@ -66,7 +66,7 @@ public class ArrayList implements List {
     /*Your code here */
       for (int i = 0; i < size; i ++){
           if (this.elts[i]> elt){
-              if (this.elts[i]<elt){
+              if (this.elts[i-1]<elt){
                   this.elts[i] = elt;
               }
 
